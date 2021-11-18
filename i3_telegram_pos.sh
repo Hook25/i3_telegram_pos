ce86d43de745fee4a89c3a4426b384e35905259f
h25 <>
2021-11-18 14:58:13 +0100

    Init script

---
+++ i3_telegram_pos.sh
@@ -0,0 +1,8 @@
+res=$(xwininfo -root | grep -oP '(?:Width:\s+)(\d+)|(?:Height:\s+)(\d+)' | sed 's/Height://g' | sed 's/Width://g' | sed ':a;N;$!ba;s/\n/ /g')
+wh=(${res//;/ })
+w=${wh[0]}
+h=${wh[1]}
+pw=`expr $w - 400`
+ph=`expr $h - 460 - 39`
+
+i3-msg "[class=TelegramDesktop] move position $pw $ph"
